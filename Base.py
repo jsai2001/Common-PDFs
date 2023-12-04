62103c099e71a6c213cfc22a6cc73b4e2b19f450
jsai2001 <121824280+jsai2001@users.noreply.github.com>
2023-12-05 03:09:20 +0530

    Update Base.py

--- Base.py
+++ Base.py
@@ -8,6 +8,15 @@ Created on Mon Nov 13 14:44:57 2023
 import os
 import random
 import shutil
+import subprocess
+
+def file_delete_if_exists_and_create(directory):
+    if os.path.isfile(directory) or os.path.islink(directory):
+        os.unlink(directory)
+    elif os.path.isdir(directory):
+        shutil.rmtree(directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 #----------------------------------------------------------------------------
@@ -44,14 +53,12 @@ import shutil
 # folder_2 = "D:\\Education\\Psychology_Chapter_2"
 # folders = [folder_1,folder_2]
 # dest_folder = "D:\\Education\\Psychology"
-# os.makedirs(dest_folder)
+# file_delete_if_exists_and_create(dest_folder)
 # for folder in folders:
 #     files = os.listdir(folder)
 #     for file in files:
 #         shutil.move(os.path.join(folder, file), dest_folder)
-#----------------------------------------------------------------------------
 
-#----------------------------------------------------------------------------
 # # Specify the path of the folder containing the images
 # root_folder = "D:\\Education\\Psychology"
 # nfolders=len(os.listdir(root_folder))
@@ -72,13 +79,11 @@ import shutil
 #         # Rename the file
 #         os.rename(old_name, new_name)
 #         file_num+=1
-#----------------------------------------------------------------------------
 
-#---------------------------------------------------------------------
 # glob = 1
 # root_folder = "D:\\Education\\Psychology"
 # dest = 'D:\\Education\\TimePass'
-# os.makedirs(dest)
+# file_delete_if_exists_and_create(dest)
 # nfolders=len(os.listdir(root_folder))
 # for i in range(1, nfolders+1):
 #     source = 'D:\\Education\\Psychology\\Folder_'+str(i)
@@ -89,25 +94,64 @@ import shutil
 #             dst_file = os.path.join(dest, file)
 #             glob += 1
 #             shutil.copy(src_file, dst_file)
-#--------------------------------------------------------------------
 
-#--------------------------------------------------------------------
-# def select_random_files(source_dir, dest_dir, n):
+# def select_random_files(source_dir, dest_dir, sample_size):
 #     files = os.listdir(source_dir)
-#     random_files = random.sample(files, n)
+#     random_files = random.sample(files, sample_size)
 
+#     file_delete_if_exists_and_create(dest_dir)
+
+#     for file in random_files:
+#         shutil.copy(os.path.join(source_dir, file), dest_dir)
+
+# source_dir = 'D:\\Education\\TimePass'
+# n_samples = int(input("Number of modules needed: "))
+# sample_size = int(input("Sample Size: "))
+# for sample_no in range(1,n_samples+1):
+#     dest_dir = f"D:\\Education\\Sample_{sample_no}"
+#     select_random_files(source_dir, dest_dir, sample_size)
+#--------------------------------------------------------------------
+# def get_all_file_paths(directory):
+#     # Initialize an empty list to store the file paths
+#     file_paths = []
+
+#     # Use os.walk to iterate over all subdirectories and files in the directory
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             if(file.endswith('.mp4')):
+#                 # Use os.path.join to construct the full path to the file
+#                 file_path = os.path.join(root, file)
+#                 # Add the file path to the list
+#                 file_paths.append(file_path)
+
+#     return file_paths
+
+# # Specify the root directory here
+# directory = 'D:\\Education'
+# file_paths = get_all_file_paths(directory)
+
+# # Print all file paths
+# for file_path in file_paths:
+#     print(file_path)
+
+# # Open the video file with the default application
+# n=int(input("1 => Call one & 2 => Filter N : "))
+# if(n==1):
+#     # Choose a random video file
+#     video_file = random.choice(file_paths)
+#     # Construct the full file path
+#     video_path = os.path.join(directory, video_file)
+#     subprocess.call(['C:\\Program Files\\VideoLAN\\VLC\\vlc.exe', video_path])
+# else:
+#     dest_dir = "D:\\Education\\Sample"
+#     n_samples = int(input("N Samples you need?: "))
 #     if os.path.isfile(dest_dir) or os.path.islink(dest_dir):
 #         os.unlink(dest_dir)
 #     elif os.path.isdir(dest_dir):
 #         shutil.rmtree(dest_dir)
 #     if not os.path.exists(dest_dir):
 #         os.makedirs(dest_dir)
-
+#     random_files = random.sample(file_paths, n_samples)
 #     for file in random_files:
-#         shutil.copy(os.path.join(source_dir, file), dest_dir)
-
-# source_dir = 'D:\\Education\\TimePass'
-# dest_dir = 'D:\\Education\\Sample'
-# n = 60
-# select_random_files(source_dir, dest_dir, n)
+#        shutil.copy(file, dest_dir)
 #--------------------------------------------------------------------
