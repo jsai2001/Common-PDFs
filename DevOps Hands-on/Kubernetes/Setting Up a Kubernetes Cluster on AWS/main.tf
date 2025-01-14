935ca41615ed9ff9faa08776712a565b46cba021
Jeevan <jeevansaikanaparthi@gmail.com>
2025-01-14 12:57:08 +0530

    Setting up Kubernetes Cluster on AWS

--- DevOps Hands-on/Kubernetes/Setting Up a Kubernetes Cluster on AWS/main.tf
+++ DevOps Hands-on/Kubernetes/Setting Up a Kubernetes Cluster on AWS/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+# Network Configuration
 resource "aws_vpc" "eks_vpc" {
     cidr_block = "10.0.0.0/16"
 }
@@ -17,6 +18,7 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
+# IAM Roles and Policies for EKS
 resource "aws_iam_role" "eks_cluster_role" {
     name = "eks-cluster-role"
 
@@ -66,6 +68,7 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+# EKS Cluster
 resource "aws_eks_cluster" "my_cluster" {
     name     = "my-cluster"
     role_arn = aws_iam_role.eks_cluster_role.arn
@@ -74,7 +77,6 @@ resource "aws_eks_cluster" "my_cluster" {
         subnet_ids = aws_subnet.eks_subnet[*].id
     }
 
-    # Add tags for better resource management
     tags = {
         Name = "EKS-Cluster"
     }
@@ -85,11 +87,35 @@ resource "aws_eks_cluster" "my_cluster" {
     ]
 }
 
+# Fetch the latest EKS optimized AMI
+data "aws_ami" "eks_optimized" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${aws_eks_cluster.my_cluster.version}-v*"]
+  }
+}
+
+# Create a launch template with the custom AMI
+resource "aws_launch_template" "eks_node_launch_template" {
+  name_prefix   = "eks-node-lt-"
+  image_id      = data.aws_ami.eks_optimized.id
+  instance_type = "t3.medium"
+}
+
+# EKS Node Group using the Launch Template
 resource "aws_eks_node_group" "my_node_group" {
     cluster_name    = aws_eks_cluster.my_cluster.name
     node_group_name = "my-node-group"
     node_role_arn   = aws_iam_role.eks_node_role.arn
     subnet_ids      = aws_subnet.eks_subnet[*].id
+
+    launch_template {
+        id      = aws_launch_template.eks_node_launch_template.id
+        version = aws_launch_template.eks_node_launch_template.latest_version
+    }
 
     scaling_config {
         desired_size = 2
@@ -97,9 +123,8 @@ resource "aws_eks_node_group" "my_node_group" {
         min_size     = 1
     }
 
-    instance_types = ["t3.medium"]
-
-    capacity_type = "ON_DEMAND"  # Use on-demand instances to stay within the Free Tier
+    # Removed 'instance_types' here since it's defined in the launch template
+    capacity_type = "SPOT"
 
     labels = {
         "role" = "worker"
