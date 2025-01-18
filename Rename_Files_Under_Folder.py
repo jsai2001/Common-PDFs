5be9990333200207f9eacf87605a5c4e8ba3d846
Jeevan <jeevansaikanaparthi@gmail.com>
2025-01-18 18:25:10 +0530

    Rename Files Under Folder

--- Rename_Files_Under_Folder.py
+++ Rename_Files_Under_Folder.py
@@ -1,13 +1,19 @@
 import os
+import random
+import string
+
+def generate_random_name(length=10):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
 def rename_videos(folder_path):
     # Get a list of all files in the folder
     files = os.listdir(folder_path)
 
     # Iterate through the files and rename them
-    for index, file in enumerate(files, start=246):
-        # Construct the new filename (e.g., Video_1, Video_2, etc.)
-        new_filename = f"Video_{index}.mp4"
+    for file in files:
+        # Construct the new filename with a random name
+        new_filename = f"{generate_random_name()}.mp4"
 
         # Get the full path of the file
         old_filepath = os.path.join(folder_path, file)
@@ -16,7 +22,6 @@ def rename_videos(folder_path):
         # Rename the file
         os.rename(old_filepath, new_filepath)
 
-
 if __name__ == "__main__":
-    folder_path = "D:\\Backup\\Folder"
+    folder_path = "D:\\Utilities"
     rename_videos(folder_path)
