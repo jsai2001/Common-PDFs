e8da7fdbc2e15f472b7bef78c3cd4419e61f9033
Jeevan <jeevansaikanaparthi@gmail.com>
2025-01-20 21:13:43 +0530

    server.json updated

--- DevOps_Hands-on/Kubernetes/Deploying_Applications_on_Kubernetes(Nodejs_React_MySQL))/backend/src/server.js
+++ DevOps_Hands-on/Kubernetes/Deploying_Applications_on_Kubernetes(Nodejs_React_MySQL))/backend/src/server.js
@@ -3,31 +3,39 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the database');
-});
-
-app.get('/data', (req, res) => {
-  db.query('SELECT * FROM items', (err, results) => {
-    if (err) {
-      res.status(500).send('Error fetching data');
-      return;
-    }
-    res.json(results);
-  });
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Backend app listening at http://localhost:${port}`);
 });
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// db.connect(err => {
+//   if (err) {
+//     console.error('Error connecting to the database:', err);
+//     return;
+//   }
+//   console.log('Connected to the database');
+// });
+
+// app.get('/data', (req, res) => {
+//   db.query('SELECT * FROM items', (err, results) => {
+//     if (err) {
+//       res.status(500).send('Error fetching data');
+//       return;
+//     }
+//     res.json(results);
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Backend app listening at http://localhost:${port}`);
+// });
